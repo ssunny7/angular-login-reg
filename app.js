6613e127810597db9ad394fa822466e1f779538b
Sunny Shah <shahsunny712@gmail.com>
2018-07-01 22:27:51 -0700

    Added index page html and main app angular module

---
+++ app.js
@@ -0,0 +1,43 @@
+'use strict';
+
+angular
+.module('mainApp', ['ngRoute', 'ngCookies'])
+.config(config)
+.run(run);
+
+config.$inject = ['$routeProvider', '$locationProvider'];
+function config($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/', {
+            controller: 'HomeController',
+            templateUrl: 'home/home.view.html',
+            controllerAs: 'vm'
+        })
+        .when('/login', {
+            controller: 'LoginController',
+            templateUrl: 'login/login.view.html',
+            controllerAs: 'vm'
+        })
+        .when('register', {
+            controller: 'RegisterController',
+            templateUrl: 'register/register.controller.js',
+            controllerAs: 'vm'
+        })
+        .otherwise({redirectTo: '/login'});
+}
+
+run.$inject = ['$rootScope', '$location', '$cookies', '$http'];
+function run($rootScope, $location, $cookies, $http) {
+    $rootScope.globals = $cookies.getObject('globals') || {};
+    if ($rootScope.globals.currentUser) {
+        $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata;
+    }
+
+    $rootScope.$on('$locationChangeStart', function (event, next, current) {
+        var restrictedPage = $.inArray($location.path(), ['/login', '/register']) === -1;
+        var loggedIn = $rootScope.globals.currentUser;
+        if (restrictedPage && !loggedIn) {
+            $location.path('/login');
+        }
+    });
+}
