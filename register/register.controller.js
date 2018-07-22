c5c88947b48baf91266b0b31bda2097a8d56552d
Sunny Shah <shahsunny712@gmail.com>
2018-07-21 19:49:53 -0700

    Added registration page controller and view

---
+++ register/register.controller.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular
+.module('mainApp')
+.controller('RegisterController', RegisterController);
+
+RegisterController.$inject = ['UserService', '$location', '$rootScope', 'FlashService'];
+function RegisterController(UserService, $location, $rootScope, FlashService) {
+    var vm = this;
+    vm.regiser = function() {
+        vm.dataLoading = true;
+
+        UserService.Create(vm.user)
+            .then(function (response) {
+                if (response.success) {
+                    FlashService.Success('Registration successful', true);
+                    $location.path('/login');
+                } else {
+                    FlashService.Error(response.message);
+                    vm.dataLoading = false;
+                }
+            });
+    }
+}
